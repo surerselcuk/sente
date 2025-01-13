c15a49a5795ab085925042f128b75c8775197433
surer <surerselcuk@gmail.com>
2025-01-13 17:11:25 +0300

    save

--- assets/sample_project/tests/sample_test.js
+++ assets/sample_project/tests/sample_test.js
@@ -1,16 +1,17 @@
-/* TEST DEFINATIONS 
-********************************************************************************************/    
-test_name = '<senteTestName> SAMPLE TEST </senteTestName>';  /* [Mandatory field] */    
+/*TEST DEFINATIONS 
+*******************/    
+test_name = '<senteTestName> SAMPLE TEST 1 </senteTestName>';  /* [Mandatory field] */    
 test_type = '<senteTestType> web-gui </senteTestType>';  /* [Mandatory field] | options: [web-gui, backend, performance] */
 
+
 /* LIBRARIES
-********************************************************************************************/
+*************/
 const { sente, helper, repo, https, Promise } = require('#libraries');
 const { testFlow, argv, jump, log, wait, go } = sente;
 
 
 /* TEST FLOW
-********************************************************************************************/
+*************/
 test = async () => {
    
     await go('https://v17.angular.io/guide')
