d5aec84ecb9e43ecbbcfa130cb96e634dbd53c46
surer <surerselcuk@gmail.com>
2025-01-15 01:48:34 +0300

    save

--- assets/sample_project/app.js
+++ assets/sample_project/app.js
@@ -1,10 +1,11 @@
-
-let app = {};
-
-module.exports = {};
-
 global.senteConfig.languages = require('./languages');
-global.environments = require('./environments');
-global.repo = require('./object_repository');
-global.helper = require('#helpers');
 global.senteConfig.testRunProjectPath = require('path').resolve(__dirname, '.') ;  // this project path
+global.environments = require('./environments');
+global.repo = require('#object_repository');
+global.helper = require('#helpers');
+
+module.exports = {
+    helper          : require('#helpers'),
+    repo            : require('./object_repository')
+
+};
