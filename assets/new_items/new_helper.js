6783208589c3c708191c9db47c62b24209787098
surerselcuk <surerselcuk@gmail.com>
2025-02-18 14:55:45 +0300

    save

--- assets/new_items/new_helper.js
+++ assets/new_items/new_helper.js
@@ -4,7 +4,7 @@ const repo = require('#object_repository');
 const { sente } = require('#libraries');
 const { webdriver, axios, Promise, https } = sente;
 const { log, now, wait, wait_, translate, importParameter, exportParameter, overrideRepo, myQuery, pgQuery, api } = sente;
-const { go, click, rightClick, see, notSee, write, keyboard, scroll } = sente;
+const { go, click, rightClick, see, notSee, write, keyboard, scroll, getText } = sente;
 
 
 
