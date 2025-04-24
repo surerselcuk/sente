0584238c8743867911193b960c68b9abe1bf236c
surerselcuk <surerselcuk@gmail.com>
2025-04-24 13:43:03 +0300

    save

--- assets/sample_project/environments/production/global.configs.js
+++ assets/sample_project/environments/production/global.configs.js
@@ -1,20 +1,31 @@
 /* default parameters:
 
-        # This parameter determines how many times the test run will be re-run if the test case receives an error. If it is zero, the test will not be re-run in case of an error.
-        number_of_test_run_repetitions_on_error : 0
+    # This parameter determines how many times the test run will be re-run if the test case encounters an error. If it is zero, the test will not be re-run in case of an error.
+    number_of_test_run_repetitions_on_error : 0
 
-        # download_path, default download directory path
-        download_path                   : path.join(senteConfig.testRunProjectPath,'files','downloads')
+    # download_path, default download directory path
+    download_path                   : path.join(senteConfig.testRunProjectPath,'files','downloads')
 
-        # screenshot_directory, default screenshot download directory path fro web-gui tests
-        screenshot_directory            : path.join(senteConfig.testRunProjectPath,'files','screen_shots')
+    # screenshot_directory, default screenshot download directory path for web-gui tests
+    screenshot_directory            : path.join(senteConfig.testRunProjectPath,'files','screen_shots')
 
-        # current_language, keyword for translate languge
-        current_language                : 'en'
+    # current_language, keyword for translation language
+    current_language                : 'en'
 
-        # sente_timeout, [seconds] If the test is not completed within this period, the test will be automatically terminated as failed.
-        sente_timeout                   : 60*10
+    # sente_timeout, [seconds] If the test is not completed within this period, the test will be automatically terminated as failed.
+    sente_timeout                   : 60*10
 
+    # browser_type, for web-gui type tests [options: 'firefox, chrome etc.]
+    browser_type                    : 'firefox'
+
+    # driver_host: current driver URL 
+
+    # This parameter specifies whether a new driver session should always be started during web-gui tests. 
+    # If true, a new driver session is always started before the test begins. 
+    # If false, the existing driver session is used.
+    # default: false
+    always_new_web_gui_session      : false
+    
 */
 
 
@@ -33,7 +44,10 @@ module.exports = {
     /* driver_host: current driver url */
     driver_host                     : 'http://127.0.0.1:4101',
 
-
+    /* This parameter specifies whether a new driver session should always be started during web-gui tests. 
+       If true, a new driver session is always started before the test begins. 
+       If false, the existing driver session is used. */
+    always_new_web_gui_session      : true
 
 
 }
